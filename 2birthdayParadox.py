144841a618d573df6b69cdf245cce387220f1ddb
Agata <agata.wycislik97@gmail.com>
2023-01-04 14:42:24 +0100

    User initial commit

--- 2birthdayParadox.py
+++ 2birthdayParadox.py
@@ -4,7 +4,8 @@ More info at https://en.wikipedia.org/wiki/Birthday_problem
 View this code at https://nostarch.com/bid-book-small-python-projects
 Tags: short, math, simulation"""
 
-import datetime, random
+import datetime
+import random
 
 
 def getBirthdays(numberOfBirthdays):
@@ -65,3 +66,44 @@ for i, birthday in enumerate(numBDays):
     if i != 0:
         # Display a comma for each birtday after the first birtday.
         print(',', end='')
+    monthName = MONTHS[birthday.month -1]
+    dateText = '{} {}'.format(monthName, birthday.day)
+    print(dateText, end='')
+print()
+print()
+
+# Determine if there are two birthdays that match.
+match = getMatch(birthdays)
+
+# Display the results:
+print('In this simulation, ', end='')
+if match != None:
+    monthName = MONTHS[match.month -1]
+    dateText = '{} {}'.format(monthName, match.day)
+    print('multiple people have a birtday on', dateText)
+else:
+    print('there are no matching birthdays.')
+print()
+
+# Run through 100,000 simulations:
+print('Generating', numBDays, 'random birthdays 100,000 times...')
+input('Press Enter to begin...')
+
+print("Let/'s run another 100,000 simulations. ")
+simMatch = 0  # How many simulations had matching birthdaus in them.
+for i in range(100_000):
+    # Report on the progers every 10,000 simulations:
+    if i % 10_000 == 0:
+        print(i, 'simulations run...')
+    birthdays = getBirthdays(numBDays)
+    if getMatch(birthdays) != None:
+        simMatch = simMatch + 1
+print('100,000 simulations run.')
+
+# Display simulation results:
+probability = round(simMatch / 100_000 * 100, 2)
+print('Out if 100,000 simulations of', numBDays, 'people, there was a')
+print('matching birthday in that group', simMatch, 'times. This means')
+print('that', numBDays, 'people have a ', probability, '% chance of')
+print('having a matching birthday in their group.')
+print("That's probably more than you would think!")
