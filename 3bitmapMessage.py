e09920a0ed423c61a5a1ee84d6373118eddec143
Agata <agata.wycislik97@gmail.com>
2023-01-11 13:35:11 +0100

    User initial commit

---
+++ 3bitmapMessage.py
@@ -0,0 +1,12 @@
+"""Bitmap Message, by Al Sweigart al@inventwithpython.com
+Displays a text message according to the provided bitmap imege.
+View this code at http://nostarch.com/big-book-small-python-projects
+Tags: tiny, beginner, artistic"""
+
+import sys
+
+# (!) Try changing this multiple string to any image you like:
+
+# There are 68 periods along the top and bottom of this string:
+# (You can also copy and paste this string from
+# https://inventwithpython.com/bitmapworld.txt)
