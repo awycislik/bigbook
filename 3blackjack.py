d734a7daea0cdd17e7f2d59139feb208264e004d
Agata <agata.wycislik97@gmail.com>
2023-01-12 22:45:21 +0100

    User initial commit

---
+++ 3blackjack.py
@@ -0,0 +1,40 @@
+"""Blackjack, by Al Sweigart al@inventwithpython.com
+2. The classic card game also known as 21. (This version doesn't have
+3. splitting or insurance.)
+4. More info at: https://en.wikipedia.org/wiki/Blackjack
+5. View this code at https://nostarch.com/big-book-small-python-projects
+6. Tags: large, game, card game"""
+
+import random, sys
+
+# Set up the constants:
+HEARTS = chr(9829)  # Characters 9829 is '♥'.
+DIAMONDS = chr(9830)  # Character 9830 is '♦'.
+SPADES = chr(9824)  # Character 9824 is '♠'
+CLUBS = chr(9827)  # Character 9827 is '♣'
+# (A list of chr codes is at https://inventwithpython.com/charactermap)
+BACKSIDE = 'backside'
+
+
+def main():
+    print('''Blackjack, by Al Sweigart
+    
+    Rules:
+        Try to get as close to 21 without going over.
+        Kings, Queens, and Jacks are worth 10 points.
+        Aces are worth 1 or 11 points.
+        Cards 2 through 10 are worth their face value.
+        (H)it to take another card.
+        (S)tand to stop taking card.
+        On your first play, you can (D)ouble down to increase your bet
+        but must hit exactly one more time before standing.
+        In case of a tie, the bet is returned to the player.
+        The dealer stops hitting at 17.''')
+    money = 5000
+    while True: # Main game loop
+        # Check if the player has run out of money:
+        if money <=0:
+            print("You're broke!")
+            print("Good thing you weren't playing with real money.")
+            print("Thanks for playing!")
+            sys.exit()
