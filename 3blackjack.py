07c04bcd8f2505cd9fcaa1f1dd91c253afced37e
Agata <agata.wycislik97@gmail.com>
2023-01-16 20:56:00 +0100

    User initial commit

--- 3blackjack.py
+++ 3blackjack.py
@@ -59,4 +59,183 @@ def main():
             if get_hand_value(player_hand) > 21:
                 break
 
-            # Get
+            # Get the player's move, either H, S or D:
+            move = get_move(player_hand, money - bet)
+
+            # Handle the player actions:
+            if move == 'D':
+                # Player is doubling down, they can increase their bet:
+                additional_bet = get_bet(min(bet, (money - bet)))
+                bet += additional_bet
+                print('Bet increased to {}.'.format(bet))
+                print("Bet:", bet)
+
+            if move in ('H', 'D'):
+                # Hit/doubling down takes another card.
+                new_card = deck.pop()
+                rank, suit = new_card
+                print('You drew a {} of {}.'.format(rank, suit))
+                player_hand.append(new_card)
+
+                if get_hand_value(player_hand) > 21:
+                    # The player has busted:
+                    continue
+
+            if move in ('S', 'D'):
+                # Stand/doubling down stops the player's turn.
+                break
+
+        # Handle the dealer's actions:
+        if get_hand_value(player_hand) <=21:
+            while get_hand_value(dealer_hand) < 17:
+                # The dealer hits:
+                print('Dealer hits...')
+                dealer_hand.append(deck.pop())
+                display_hands(player_hand, dealer_hand, False)
+
+                if get_hand_value(dealer_hand) > 21:
+                    break  # The dealer has busted.
+                input('Press Enter to continue...')
+                print('\n\n')
+
+        # Show the final hands:
+        display_hands(player_hand, dealer_hand, True)
+
+        player_value = get_hand_value(player_hand)
+        dealer_value = get_hand_value(dealer_hand)
+        # Handle whether the player won, lost or tied:
+        if dealer_value > 21:
+            print('Dealer busts! You win ${}!'.format(bet))
+            money += bet
+        elif (player_value > 21) or (player_value < dealer_value):
+            print('You lost!')
+            money -= bet
+        elif player_value > dealer_value:
+            print('You won ${}!'.format(bet))
+            money += bet
+        elif player_value == dealer_value:
+            print("It's a tie, the bet is returned to you.")
+
+        input('Press Enter to continue...')
+        print('\n\n')
+
+
+def get_bet(max_bet):
+    """Ask the player how much they want to bet for this round."""
+    while True:  # Keep asking until they enter a valid amount.
+        print('How much do you bet? (1-{}, or QUIT)'.format(max_bet))
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Thanks for playing!')
+            sys.exit()
+
+        if not bet.isdecimal():
+            continue  # If the player didn't enter a number, ask again.
+
+        bet = int(bet)
+        if 1 <= bet <= max_bet:
+            return bet  # Player entered a valid bet.
+
+
+def get_deck():
+    """Return a list of (rank, suit) tuples for all 52 cards."""
+    deck = []
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range(2, 11):
+            deck.append((str(rank), suit))  # Add the numbered cards.
+        for rank in ('J', 'Q', 'K', 'A'):
+            deck.append((rank, suit))  # Add the face and ace cards.
+    random.shuffle(deck)
+    return deck
+
+
+def display_hands(player_hand, dealer_hand, show_dealer_hand):
+    """Show the player's and dealer's cards. Hide the dealer's first
+    card if show_dealer_hand is False."""
+    print()
+    if show_dealer_hand:
+        print('DEALER:', get_hand_value(dealer_hand))
+        display_cards(dealer_hand)
+    else:
+        print('DEALER: ???')
+        # Hide the dealer's first card:
+        display_cards([BACKSIDE] + dealer_hand[1:])
+
+    # Show the player's cards:
+    print('PLAYER:', get_hand_value(player_hand))
+    display_cards(player_hand)
+
+
+def get_hand_value(cards):
+    """Returns the value of the cards. Face cards are worth 10, aces are
+    worth 11 or 1 (this function picks the most suitable ace value)."""
+    value = 0
+    number_of_aces = 0
+
+    # Add the value for the non-ace cards:
+    for card in cards:
+        rank = card[0]  # card is a tuple like (rank, suit)
+        if rank == 'A':
+            number_of_aces += 1
+        elif rank in ('K', 'Q', 'J'):  # Face cards are worth 10 points.
+            value += 10
+        else:
+            value += int(rank) # Numbered cards are worth their number.
+
+    # Add the value for the aces:
+    value += number_of_aces  # Add 1 per ace.
+    for i in range(number_of_aces):
+        # If another 10 can be added with busting, do so:
+        if value + 10 <= 21:
+            value += 10
+
+    return value
+
+
+def display_cards(cards):
+    """Display all the cards in the cards list."""
+    rows = ['', '', '', '', '']  # The text to display on each row.
+
+    for i, card in enumerate(cards):
+        rows[0] += '__ '  # Print the top line of the card.
+        if card == BACKSIDE:
+            # Print a card's back:
+            rows[1] += '|## |'
+            rows[2] += '|###|'
+            rows[3] += '|_##|'
+        else:
+            # Print the card's front:
+            rank, suit = card  # The card is a tuple data structure.
+            rows[1] += '|{} | '.format(rank.ljust(2))
+            rows[2] += '| {} | '.format(suit)
+            rows[3] += '|_{}| '.format(rank.rjust(2, '_'))
+
+        # Print each row on the screen:
+        for row in rows:
+            print(row)
+
+
+def get_move(player_hand, money):
+    """Asks the player for their move, and returns 'H' for hit, 'S' for
+    stand and 'D' for double down."""
+    while True:  # Keep looping until the player can make:
+        # Determine what moves the player can make
+        moves = ['(H)it', '(S)tand']
+
+        # The player can double down on their first move, which we can
+        # tell because they'll have exactly two cards:
+        if len(player_hand) == 2 and money > 0:
+            moves.append('(D)ouble down')
+
+        # Get the player's move:
+        move_prompt = ', '.join(moves) + '> '
+        move = input(move_prompt).upper()
+        if move in ('H', 'S'):
+            return move  # Player has entered a valid move.
+        if move == 'D' and '(D)ouble down' in moves:
+            return move # Player has entered a valid move.
+
+
+# If the program is run (instead of imported), run the game:
+if __name__ == "__main__":
+    main()
